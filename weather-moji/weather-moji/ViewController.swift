4005acfb696fc98cdd982e75ef34b94ddbeaaf50
hemssy <hemsyeons@gmail.com>
2025-11-03 16:31:52 +0900

    [Chore] gitignore 추가

---
+++ weather-moji/weather-moji/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weather-moji
+//
+//  Created by 신서연 on 11/3/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
