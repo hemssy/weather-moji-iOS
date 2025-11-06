3ef6a373932c7be928ff3edfca56e5d44ec899e7
zzaeun <leeje57575@gmail.com>
2025-11-06 15:53:10 +0900

    [Feat] #43 - 로고 탭 -> ForecastVC로 이동

---
+++ weather-moji/weather-moji/Features/Search/View/ForecastViewController.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class ForecastViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .white
+    }
+}
